ecf75bf72c91acd2e573927bbf3cc842a430788b
Shirupan <429379083@qq.com>
2019-08-30 16:22:29 +0800

    添加几个工具类

---
+++ MvpLibrary/lib_base/src/main/java/com/stone/baselib/utils/SMemoryUtils.java
@@ -0,0 +1,26 @@
+package com.stone.baselib.utils;
+
+/**
+ * Stone
+ * 2019/8/30
+ */
+public final class SMemoryUtils {
+
+    /**
+     * Byte与Byte的倍数
+     */
+    public static final int BYTE = 1;
+    /**
+     * KB与Byte的倍数
+     */
+    public static final int KB   = 1024;
+    /**
+     * MB与Byte的倍数
+     */
+    public static final int MB   = 1048576;
+    /**
+     * GB与Byte的倍数
+     */
+    public static final int GB   = 1073741824;
+
+}
---
+++ MvpLibrary/lib_base/src/main/java/com/stone/baselib/utils/STimeUtils.java
@@ -0,0 +1,30 @@
+package com.stone.baselib.utils;
+
+/**
+ * Stone
+ * 2019/8/30
+ */
+public final class STimeUtils {
+
+    /**
+     * 毫秒与毫秒的倍数
+     */
+    public static final int MSEC = 1;
+    /**
+     * 秒与毫秒的倍数
+     */
+    public static final int SEC = 1000;
+    /**
+     * 分与毫秒的倍数
+     */
+    public static final int MIN = 60000;
+    /**
+     * 时与毫秒的倍数
+     */
+    public static final int HOUR = 3600000;
+    /**
+     * 天与毫秒的倍数
+     */
+    public static final int DAY = 86400000;
+
+}
